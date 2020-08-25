041aa3fa6a108356d72df879c83ba90237d02406
Phil Roli <philipprolinek@gmail.com>
2020-08-25 20:05:20 +0200

    DIM open & timer reduced

--- DestinySteam.bat
+++ DestinySteam.bat
@@ -1,5 +1,8 @@
 @echo off
 
+"C:\Program Files (x86)\Google\Chrome\Application\chrome_proxy.exe" --profile-directory=Default --app-id=diekafcbbmcolffflekimginbldpnioe
+timeout /t 15 /nobreak > NUL
+
 tasklist /FI "IMAGENAME eq Destiny2.exe" 2>NUL | find /I /N "Destiny2.exe">NUL
 if "%ERRORLEVEL%"=="0" (exit) else (
     tasklist /FI "IMAGENAME eq steam.exe" 2>NUL | find /I /N "steam.exe">NUL
@@ -9,7 +12,7 @@ if "%ERRORLEVEL%"=="0" (exit) else (
         exit) else (
             cd "C:\Program Files (x86)\Steam"
             start steam.exe
-            timeout /t 30 /nobreak > NUL
+            timeout /t 15 /nobreak > NUL
             cd "C:\Program Files (x86)\Steam\steamapps\common\Destiny 2"
             start destiny2.exe
             exit
