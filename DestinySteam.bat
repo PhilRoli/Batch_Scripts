0343d049506304f1ec15acb18d21c2e0f2f80e0d
Phil Roli <philipprolinek@gmail.com>
2020-08-25 17:31:44 +0200

    Check if either Steam or D2 are running and act accoringly

--- DestinySteam.bat
+++ DestinySteam.bat
@@ -1,13 +1,16 @@
 @echo off
 
-cd "C:\Program Files (x86)\Steam"
-
-start steam.exe
-
-timeout /t 30 /nobreak > NUL
-
-cd "C:\Program Files (x86)\Steam\steamapps\common\Destiny 2"
-
-start destiny2.exe
-
-exit
+tasklist /FI "IMAGENAME eq Destiny2.exe" 2>NUL | find /I /N "Destiny2.exe">NUL
+if "%ERRORLEVEL%"=="0" (exit) else (
+    tasklist /FI "IMAGENAME eq steam.exe" 2>NUL | find /I /N "steam.exe">NUL
+    if "%ERRORLEVEL%"=="0" (
+        cd "C:\Program Files (x86)\Steam\steamapps\common\Destiny 2"
+        start destiny2.exe
+        exit) else (
+            cd "C:\Program Files (x86)\Steam"
+            start steam.exe
+            timeout /t 30 /nobreak > NUL
+            cd "C:\Program Files (x86)\Steam\steamapps\common\Destiny 2"
+            start destiny2.exe
+            exit
+    ))
