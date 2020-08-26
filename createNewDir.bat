c387a3f08e69e31e44172bd093f666d7a7ece377
Phil Roli <philipprolinek@gmail.com>
2020-08-26 04:06:56 +0200

    Added vsc running check and choice to start vsc and add DIR to workspace

--- createNewDir.bat
+++ createNewDir.bat
@@ -24,6 +24,27 @@ git commit -m "Created the %dirname% folder inkl README.md file"
 
 cd "C:\"
 
-code -a --add %dirname%
+set /p choice="Do you want to add the new DIR to VSC now? (Y/N) "
+if "%choice%" == "y" (goto CheckVSC) else (goto Done)
 
-pause
+:CheckVSC
+tasklist /FI "IMAGENAME eq Code.exe" 2>NUL | find /I /N "Code.exe">NUL
+if "%ERRORLEVEL%"=="0" (goto AddDIR) else (goto Choice)
+
+:Choice
+    set /p choice2="Do you want to start VSC now? (Y/N) "
+    if "%choice2%" == "y" (goto StartVSC) else (goto Done)
+
+:StartVSC
+    cd "C:\Users\phili\AppData\Local\Programs\Microsoft VS Code"
+    echo Visual Studio Code is Starting
+    start Code.exe
+    timeout /t 15 /nobreak > NUL
+    goto AddDIR
+
+:AddDIR
+    code -a --add %dirname%
+
+:Done
+
+exit
