0bf605e4ee118f3739bb4de756695f442960d3d9
Phil Roli <philipprolinek@gmail.com>
2020-08-26 03:30:15 +0200

    added programming & test inut added one letter answers

--- startup_programms.bat
+++ startup_programms.bat
@@ -6,18 +6,37 @@ set /p location="Where are you right now? "
 
 echo .
 
-if "%location%" == "home" (
-    launchHome.vbs
-    exit
-)
+if "%location%" == "home" goto ResultHome
+if "%location%" == "h" goto ResultHome
 
-if "%location%" == "school" (
-    launchSchool.vbs
-    exit
-)
+if "%location%" == "school" goto ResultSchool
+if "%location%" == "s" goto ResultSchool
+
+if "%location%" == "programming" goto ResultProgramming
+if "%location%" == "p" goto ResultProgramming
+
+if "%location%" == "test" goto ResultTest
+if "%location%" == "t" goto ResultTest
 
 echo Die einganbe war nicht korrekt
-
 echo .
-
 pause
+exit
+
+:ResultHome
+    launchHome.vbs
+    exit
+
+:ResultSchool
+    launchSchool.vbs
+    exit
+
+:ResultProgramming
+    launchProgramming.vbs
+    exit
+
+:ResultTest
+    echo Test Erfolgreich
+    echo .
+    pause
+    exit
