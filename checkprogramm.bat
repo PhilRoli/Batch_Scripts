e770720311e6d526e066e14a74ccc8026a4a2672
Phil Roli <philipprolinek@gmail.com>
2020-08-26 03:14:26 +0200

    added question if to kill the asked programm

--- checkprogramm.bat
+++ checkprogramm.bat
@@ -5,8 +5,20 @@ set /p programm="Programm to check: "
 echo .
 
 tasklist /FI "IMAGENAME eq %programm%.exe" 2>NUL | find /I /N "%programm%.exe">NUL
-if "%ERRORLEVEL%"=="0" (echo Program is running) else (echo Programm is not running)
+if "%ERRORLEVEL%"=="0" (echo Program is running) else (
+    echo Programm is not running
+    echo .
+    pause
+    exit
+)
 
 echo .
 
-pause
+set /p killchoice="Do you want to kill this programm? (Y/N) "
+
+echo .
+
+if "%killchoice%"=="y" (Taskkill /IM %programm%.exe /F
+    echo .
+    pause
+) else (pause)
