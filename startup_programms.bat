535d83fda8a90202102054425a74d033d75e5309
Phil Roli <philipprolinek@gmail.com>
2020-09-01 18:19:53 +0200

    Added a input function for Among Us

--- startup_programms.bat
+++ startup_programms.bat
@@ -2,7 +2,7 @@
 
 cd "C:\VSC_Batch_Scripts\vbs_files"
 
-set /p location="Home / School / Programming / Destiny: "
+set /p location="Home / School / Programming / Destiny / Among: "
 
 echo [30m.[37m
 
@@ -18,6 +18,9 @@ if "%location%" == "p" goto ResultProgramming
 if "%location%" == "destiny" goto ResultDestiny
 if "%location%" == "d" goto ResultDestiny
 
+if "%location%" == "among" goto ResultAmong
+if "%location%" == "a" goto ResultAmong
+
 if "%location%" == "test" goto ResultTest
 if "%location%" == "t" goto ResultTest
 
@@ -27,22 +30,44 @@ pause
 exit
 
 :ResultHome
+    echo Launching Home Programms
+    echo [30m.[37m
+    timeout /t 5 > NUL
     launchHome.vbs
     exit
 
 :ResultSchool
+    echo Launching School Programms
+    echo [30m.[37m
+    timeout /t 5 > NUL
     launchSchool.vbs
     exit
 
 :ResultProgramming
+    echo Launching Programming Programms
+    echo [30m.[37m
+    timeout /t 5 > NUL
     launchProgramming.vbs
     exit
 
 :ResultDestiny
+    echo Launching Destiny
+    echo [30m.[37m
+    timeout /t 5 > NUL
     launchDestiny2.vbs
     exit
+	
+:ResultAmong
+    echo Launching Among Us
+    echo [30m.[37m
+    timeout /t 5 > NUL
+	launchAmong.vbs
+	exit
 
 :ResultTest
+    echo Launching Test Programms
+    echo [30m.[37m
+    timeout /t 5 > NUL
     echo Test Erfolgreich
     echo [30m.[37m
     pause
