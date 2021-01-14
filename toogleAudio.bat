8bb95c6480f60fde888a1c73f7b1c75de4889f6d
Philipp Rolinek <philipprolinek@gmail.com>
2021-01-14 03:00:26 +0100

    Created a new project to toggle between 2 audio inputs with a single double click.

---
+++ toogleAudio.bat
@@ -0,0 +1,31 @@
+REM small file which gets the current default audio device and then triggers a shortcut which changes the default to the opposite one via SoundVolumeView
+
+REM change do directory where .exe is located
+cd C:\VSC_Batch_Scripts\soundVolumeShortcuts
+
+REM get current mute status of Headset > 0 = in use / 1 = not in use
+set /p level=<"C:\users\phili\documents\level.txt"
+
+echo %level%
+
+REM check which mute status headset has
+if "%level%"=="0 " goto switchPS5
+if "%level%"=="1 " goto switchPC
+
+:switchPS5
+
+REM set the speakers (3.5mm) as default audio
+start PS5
+
+echo 1 >"C:\users\phili\documents\level.txt"
+
+exit
+
+:switchPC
+
+REM set the headset as default audio device
+start PC
+
+echo 0 >"C:\users\phili\documents\level.txt"
+
+exit
