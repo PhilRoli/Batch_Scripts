8724a09a484d176f34a23cdb44d4f19a9acde5c6
Philipp Rolinek <philipprolinek@gmail.com>
2021-04-10 21:37:51 +0200

    New FIle, opens all 3 insances of Discord at one

---
+++ DiscordDev.bat
@@ -0,0 +1,36 @@
+@echo off
+
+echo Checking if Discord is running
+
+tasklist /FI "IMAGENAME eq Discord.exe" 2>NUL | find /I /N "Discord.exe">NUL
+if "%ERRORLEVEL%"=="0" (echo Discord is running) else (
+    cd "C:\Users\phili\AppData\Local\Discord\app-0.0.309"
+    echo Discord is Starting
+    start Discord.exe
+    timeout /t 15 /nobreak > NUL)
+
+echo [30m.[37m
+
+echo Checking if DiscordPTB is running
+
+tasklist /FI "IMAGENAME eq DiscordPTB.exe" 2>NUL | find /I /N "DiscordPTB.exe">NUL
+if "%ERRORLEVEL%"=="0" (echo DiscordPTB is running) else (
+    cd "C:\Users\phili\AppData\Local\DiscordPTB\app-1.0.1005"
+    echo DiscordPTB is Starting
+    start DiscordPTB.exe
+    timeout /t 15 /nobreak > NUL)
+
+echo [30m.[37m
+
+echo Checking if Discord is running
+
+tasklist /FI "IMAGENAME eq DiscordCanary.exe" 2>NUL | find /I /N "DiscordCanary.exe">NUL
+if "%ERRORLEVEL%"=="0" (echo DiscordCanary is running) else (
+    cd "C:\Users\phili\AppData\Local\DiscordCanary\app-1.0.28"
+    echo DiscordCanary is Starting
+    start DiscordCanary.exe
+    timeout /t 15 /nobreak > NUL)
+
+echo [30m.[37m
+
+exit
---
+++ vbs_files/lauchDiscordDev.vbs
@@ -0,0 +1,3 @@
+Set WshShell = CreateObject("WScript.Shell") 
+WshShell.Run chr(34) & "C:\VSC_Batch_Scripts\DiscordDev.bat" & Chr(34), 0
+Set WshShell = Nothing
